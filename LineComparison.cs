9a551e38d7bdc8f312270c2073744d234e170e7a
r-svp26 <riteshk6341@gmail.com>
2021-07-21 15:47:09 +0530

    [Ritesh] Refactor: Constructor are used for code readability

--- LineComparison.cs
+++ LineComparison.cs
@@ -6,33 +6,37 @@ namespace LineComparisonComputation
 {
     class LineComparison
     {
+        // variables
+        double pointX1;
+        double pointY1;
+        double pointX2;
+        double pointY2;
+        double pointX3;
+        double pointY3;
+        double pointX4;
+        double pointY4;
+
+        public LineComparison(double pointX1, double pointY1, double pointX2, double pointY2,
+                                double pointX3, double pointY3, double pointX4, double pointY4) 
+        {
+            this.pointX1 = pointX1;
+            this.pointY1 = pointY1;
+            this.pointX2 = pointX2;
+            this.pointY2 = pointY2;
+            this.pointX3 = pointX3;
+            this.pointY3 = pointY3;
+            this.pointX4 = pointX4;
+            this.pointY4 = pointY4;
+        }
+
         /// <summary>
-        /// This method is used to check two lines are equal or not.
+        /// This method is used to check which line is greater, less or both lines are equal.
         /// </summary>
         public void length() 
         {
             // variables
-            double pointX1;
-            double pointY1;
-            double pointX2;
-            double pointY2;
-            double pointX3;
-            double pointY3;
-            double pointX4;
-            double pointY4;
             double distance1;
             double distance2;
-
-            Console.WriteLine("Enter the first line co-ordinates");
-            pointX1 = Convert.ToDouble(Console.ReadLine());
-            pointY1 = Convert.ToDouble(Console.ReadLine());
-            pointX2 = Convert.ToDouble(Console.ReadLine());
-            pointY2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the second line co-ordinates");
-            pointX3 = Convert.ToDouble(Console.ReadLine());
-            pointY3 = Convert.ToDouble(Console.ReadLine());
-            pointX4 = Convert.ToDouble(Console.ReadLine());
-            pointY4 = Convert.ToDouble(Console.ReadLine());
 
             // length of first line
             distance1 = Math.Sqrt((pointX2 - pointX1) * (pointX2 - pointX1) + (pointY2 - pointY1) * (pointY2 - pointY1));
@@ -41,11 +45,11 @@ namespace LineComparisonComputation
             distance2 = Math.Sqrt((pointX4 - pointX3) * (pointX4 - pointX3) + (pointY4 - pointY3) * (pointY4 - pointY3));
             Console.WriteLine("Length of a line:" + distance2);
 
-            // comparing the two lines
+            // comparing two lines
             if (distance1.CompareTo(distance2) > 0)
-                Console.WriteLine("The length of first is greater than second line.");
+                Console.WriteLine("The length of first line is greater than second line.");
             else if (distance1.CompareTo(distance2) < 0)
-                Console.WriteLine("The length of first is less than second line.");
+                Console.WriteLine("The length of first line is less than second line.");
             else
                 Console.WriteLine("Both the lines are equal.");
 
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace LineComparisonComputation
     {
         static void Main(string[] args)
         {
-            LineComparison lineComparison = new LineComparison();
+            LineComparison lineComparison = new LineComparison(1.2,2.3,4.5,3.2,3.4,2.6,5.2,6.1);
             lineComparison.length();
         }
     }

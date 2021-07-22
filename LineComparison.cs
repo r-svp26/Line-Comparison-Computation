0501ac07183364227b8b86977876a86400032803
r-svp26 <riteshk6341@gmail.com>
2021-07-22 14:39:13 +0530

    [Ritesh] Refactor: CheckLine method is implemented

--- LineComparison.cs
+++ LineComparison.cs
@@ -7,52 +7,20 @@ namespace LineComparisonComputation
     class LineComparison
     {
         // variables
-        double pointX1;
-        double pointY1;
-        double pointX2;
-        double pointY2;
-        double pointX3;
-        double pointY3;
-        double pointX4;
-        double pointY4;
-
-        public LineComparison(double pointX1, double pointY1, double pointX2, double pointY2,
-                                double pointX3, double pointY3, double pointX4, double pointY4) 
+        double pointX1, pointY1, pointX2, pointY2;
+        public LineComparison(double pointX1, double pointY1, double pointX2, double pointY2)                  
         {
             this.pointX1 = pointX1;
             this.pointY1 = pointY1;
             this.pointX2 = pointX2;
             this.pointY2 = pointY2;
-            this.pointX3 = pointX3;
-            this.pointY3 = pointY3;
-            this.pointX4 = pointX4;
-            this.pointY4 = pointY4;
         }
-
         /// <summary>
         /// This method is used to check which line is greater, less or both lines are equal.
         /// </summary>
-        public void length() 
+       public double Length() 
         {
-            // variables
-            double distance1;
-            double distance2;
-
-            // length of first line
-            distance1 = Math.Sqrt((pointX2 - pointX1) * (pointX2 - pointX1) + (pointY2 - pointY1) * (pointY2 - pointY1));
-            Console.WriteLine("Length of a line:" +distance1);
-            // length of first line
-            distance2 = Math.Sqrt((pointX4 - pointX3) * (pointX4 - pointX3) + (pointY4 - pointY3) * (pointY4 - pointY3));
-            Console.WriteLine("Length of a line:" + distance2);
-
-            // comparing two lines
-            if (distance1.CompareTo(distance2) > 0)
-                Console.WriteLine("The length of first line is greater than second line.");
-            else if (distance1.CompareTo(distance2) < 0)
-                Console.WriteLine("The length of first line is less than second line.");
-            else
-                Console.WriteLine("Both the lines are equal.");
-
-        }
+            return Math.Sqrt((pointX2 - pointX1) * (pointX2 - pointX1) + (pointY2 - pointY1) * (pointY2 - pointY1));
+        } 
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,30 @@ namespace LineComparisonComputation
 {
     class Program
     {
+        // variables
+        static double distance1, distance2;
+
+        /// <summary>
+        /// This method is used to check line status.
+        /// </summary>
+        public static void CheckLine()
+        {
+            LineComparison lineComparison1 = new LineComparison(1.2, 2.3, 4.5, 3.2);
+            distance1 = lineComparison1.Length();
+            LineComparison lineComparison2 = new LineComparison(1.0, 2.5, 3.5, 1.2);
+            distance2 = lineComparison2.Length();
+            // used compareto method to check the line greater,less or both are equals.
+            if (distance1.CompareTo(distance2) > 0)
+                Console.WriteLine("The length of first line is greater than second line.");
+            else if (distance1.CompareTo(distance2) < 0)
+                Console.WriteLine("The length of first line is less than second line.");
+            else
+                Console.WriteLine("Both the lines are equal.");
+        }
+
         static void Main(string[] args)
         {
-            LineComparison lineComparison = new LineComparison(1.2,2.3,4.5,3.2,3.4,2.6,5.2,6.1);
-            lineComparison.length();
+            Program.CheckLine();
         }
     }
 }
